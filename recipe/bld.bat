2741acbfe2a8361fe891650d810fa410254907b0
Jean-Christophe Morin <jcmorin@anaconda.com>
2023-06-16 14:49:47 -0400

    Don't build tests

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,6 +6,7 @@ cmake ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DBUILD_TESTS=OFF ^
     ..
 
 cmake --build . --target install --config Release
